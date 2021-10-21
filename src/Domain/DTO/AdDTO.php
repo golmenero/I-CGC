1dc0d8fe9a0d41418739d4d0f6cfa955087a5ca2
golmenero <charlygomezcolmenero@gmail.com>
2021-10-21 10:50:29 +0200

    Documentation for the DTOS

--- src/Domain/DTO/AdDTO.php
+++ src/Domain/DTO/AdDTO.php
@@ -6,6 +6,10 @@ namespace Domain\DTO;
 
 use DateTimeImmutable;
 
+/**
+ * Class AdDTO
+ * This is the DTO (Data Transfer Object) for the Ad Object
+ */
 final class AdDTO
 {
     private int $id;
@@ -20,6 +24,17 @@ final class AdDTO
     private int $points;
 
 
+    /**
+     * Constructor of the class AdDTO 
+     * @param int $id
+     * @param String $typology
+     * @param String $description
+     * @param array $pictures
+     * @param int $houseSize
+     * @param int $gardenSize
+     * @param int $score
+     * @param DateTimeImmutable $irrelevantSince
+     */
     public function __construct($id, $typology, $description, $pictures, $houseSize, $gardenSize, $score, $irrelevantSince)
     {
         $this->points = 0;
@@ -33,97 +48,172 @@ final class AdDTO
         $this->irrelevantSince = $irrelevantSince;
     }
 
+    /**
+     * Getter for the parameter Id in class AdDTO
+     * @return int ID
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Setter for the parameter Id in class AdDTO
+     * @param int $idP The new Id
+     */
     public function setId($idP)
     {
         $this->id = $idP;
     }
 
+    /**
+     * Getter for the parameter Typology in class AdDTO
+     * @return String typology
+     */
     public function getTypology()
     {
         return $this->typology;
     }
 
+    /**
+     * Setter for the parameter Typology in class AdDTO
+     * @param String $typologyP The new Typology
+     */
     public function setTypology($typologyP)
     {
         $this->typology = $typologyP;
     }
 
+    /**
+     * Getter for the parameter Description in class AdDTO
+     * @return String description
+     */
     public function getDescription()
     {
         return $this->description;
     }
 
+    /**
+     * Setter for the parameter Description in class AdDTO
+     * @param String $descriptionP The new Description
+     */
     public function setDescription($descriptionP)
     {
         $this->description = $descriptionP;
     }
 
+    /**
+     * Getter for the parameter Pictures in class AdDTO
+     * @return Array[int] pictures
+     */
     public function getPictures()
     {
         return $this->pictures;
     }
 
+    /**
+     * Setter for the parameter Pictures in class AdDTO
+     * @param Array[int] $picturesP The new Pictures
+     */
     public function setPictures($picturesP)
     {
         $this->pictures = $picturesP;
     }
 
+    /**
+     * Getter for the parameter HouseSize in class AdDTO
+     * @return int houseSize
+     */
     public function getHouseSize()
     {
         return $this->houseSize;
     }
 
+    /**
+     * Setter for the parameter HouseSize in class AdDTO
+     * @param int $houseSizeP The new houseSize
+     */
     public function setHouseSize($houseSizeP)
     {
         $this->houseSize = $houseSizeP;
     }
 
+    /**
+     * Getter for the parameter GardenSize in class AdDTO
+     * @return int gardenSize
+     */
     public function getGardenSize()
     {
         return $this->gardenSize;
     }
 
+    /**
+     * Setter for the parameter GardenSize in class AdDTO
+     * @param int $gardenSizeP The new gardenSize
+     */
     public function setGardenSize($gardenSizeP)
     {
         $this->gardenSize = $gardenSizeP;
     }
 
+    /**
+     * Getter for the parameter Score in class AdDTO
+     * @return int score
+     */
     public function getScore()
     {
         return $this->score;
     }
 
+    /**
+     * Setter for the parameter Score in class AdDTO
+     * @param int $scoreP The new score
+     */
     public function setScore($scoreP)
     {
         $this->score = $scoreP;
     }
 
+    /**
+     * Getter for the parameter IrrelevantSince in class AdDTO
+     * @return DateTimeImmutable irrelevantSince
+     */
     public function getIrrelevantSince()
     {
         return $this->irrelevantSince;
     }
 
+    /**
+     * Setter for the parameter irrelevantSince in class AdDTO
+     * @param DateTimeImmutable $irrelevantSinceP The new irrelevantSince
+     */
     public function setIrrelevantSince($irrelevantSinceP)
     {
         $this->irrelevantSince = $irrelevantSinceP;
     }
 
-
+    /**
+     * Getter for the parameter Points in class AdDTO
+     * @return int points
+     */
     public function getPoints()
     {
         return $this->points;
     }
 
+    /**
+     * Setter for the parameter Score in class AdDTO
+     * @param int $scoreP The new score
+     */
     public function setPoints($newP)
     {
         $this->points = $newP;
     }
 
+    /**
+     * This function updates the points of the object AdDTO
+     * @param int $newP The number of points to update
+     */
     public function increasePoints($newP)
     {
         $this->points += $newP;
--- src/Domain/DTO/PictureDTO.php
+++ src/Domain/DTO/PictureDTO.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace Domain\DTO;
 
+/**
+ * Class PictureDTO
+ * This is the DTO (Data Transfer Object) for the Picture Object
+ */
 final class PictureDTO
 {
     private int $id;
     private String $url;
     private String $quality;
 
+    /**
+     * Constructor of the class PictureDTO
+     * @param int $id
+     * @param String $url
+     * @param String $quality
+     */
     public function __construct($id, $url, $quality)
     {
         $this->id = $id;
@@ -17,31 +27,55 @@ final class PictureDTO
         $this->quality = $quality;
     }
 
+    /**
+     * Getter for the parameter Id in class PictureDTO
+     * @return int ID
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Setter for the parameter Id in class PictureDTO
+     * @param int $idP The new Id
+     */
     public function setId($idP)
     {
         $this->id = $idP;
     }
 
+    /**
+     * Getter for the parameter Url in class PictureDTO
+     * @return String url
+     */
     public function getUrl()
     {
         return $this->url;
     }
 
+    /**
+     * Setter for the parameter Url in class PictureDTO
+     * @param String $urlP The new url
+     */
     public function setUrl($urlP)
     {
         $this->url = $urlP;
     }
 
+    /**
+     * Getter for the parameter Quality in class PictureDTO
+     * @return String quality
+     */
     public function getQuality()
     {
         return $this->quality;
     }
 
+    /**
+     * Setter for the parameter Quality in class PictureDTO
+     * @param String $qualityP The new quality
+     */
     public function setQuality($qualityP)
     {
         $this->quality = $qualityP;
